d801c7c5d726ef7fdfb5e42ec44f6a5553e47e88
Tyris <68648633+Tyris0001@users.noreply.github.com>
2024-12-13 09:41:24 +0100

    Finished tests

--- trafficlight.test.js
+++ trafficlight.test.js
@@ -4,64 +4,91 @@ const app = 'http://localhost:3000';
 describe("GET /api/welcome", () => {
     it("welcome message", async () => {
         return request(app)
-        .get("/api/welcome")
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect(function (res) {
-            if (!res.body.hasOwnProperty('status')) {
-                throw new Error("Expect 'status' key!");
-            }
-            if (!res.body.hasOwnProperty('message')) {
-                throw new Error("Expect 'message' key!")
-            }
-        })
-        .then((res) =>{
-            expect(res.statusCode).toBe(200);
-        })
+            .get("/api/welcome")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(function (res) {
+                if (!res.body.hasOwnProperty('status')) {
+                    throw new Error("Expect 'status' key!");
+                }
+                if (!res.body.hasOwnProperty('message')) {
+                    throw new Error("Expect 'message' key!")
+                }
+            })
+            .then((res) => {
+                expect(res.statusCode).toBe(200);
+            })
     });
 });
 
 describe("GET /api/trafficlight", () => {
     it("out of order", async () => {
         return request(app)
-        .get("/api/trafficlight")
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect(function (res) {
-            if (!res.body.hasOwnProperty('status')) {
-                throw new Error("Expect 'status' key!");
-            }
-            if (!res.body.hasOwnProperty('message')) {
-                throw new Error("Expect 'message' key!")
-            }
-        })
-        .then((res) => {
-            expect(res.body.message).toBe('OUT OF ORDER')
-        })
+            .get("/api/trafficlight")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(function (res) {
+                if (!res.body.hasOwnProperty('status')) {
+                    throw new Error("Expect 'status' key!");
+                }
+                if (!res.body.hasOwnProperty('message')) {
+                    throw new Error("Expect 'message' key!")
+                }
+            })
+            .then((res) => {
+                expect(res.body.message).toBe('OUT OF ORDER')
+            })
     });
 });
 
 describe("GET /api/trafficlight/:color", () => {
     it("red", async () => {
-        
+        return request(app)
+            .get("/api/trafficlight/red")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(function (res) {
+                if (!res.body.hasOwnProperty('status')) {
+                    throw new Error("Expect 'status' key!");
+                }
+                if (!res.body.hasOwnProperty('message')) {
+                    throw new Error("Expect 'message' key!")
+                }
+            })
+            .then((res) => {
+                expect(res.body.message).toBe('DON\'T WALK!')
+            })
     });
 
     it("green", async () => {
-//??
-//??
-//??
-//??
-//??
-//??
-//??
+        return request(app)
+            .get("/api/trafficlight/green")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(function (res) {
+                if (!res.body.hasOwnProperty('status')) {
+                    throw new Error("Expect 'status' key!");
+                }
+                if (!res.body.hasOwnProperty('message')) {
+                    throw new Error("Expect 'message' key!")
+                }
+            })
+            .then((res) => {
+                expect(res.body.message).toBe('WALK!')
+            })
     });
 
     it("orange", async () => {
-//??
-//??
-//??
-//??
-//??
-//??
-//??
+        return request(app)
+            .get("/api/trafficlight/orange")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(function (res) {
+                if (!res.body.hasOwnProperty('status')) {
+                    throw new Error("Expect 'status' key!");
+                }
+                if (!res.body.hasOwnProperty('message')) {
+                    throw new Error("Expect 'message' key!")
+                }
+            })
+            .then((res) => {
+                expect(res.body.message).toBe('ATTENTION!')
+            })
     });
 });
 
